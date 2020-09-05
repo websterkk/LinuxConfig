d987f3aa0aee41001a9465417b1f4a8e7b8deae6
kjw <kjw@tuta.io>
2020-09-05 14:49:35 -0600

    updating dot files

---
+++ .scripts/screenres/scripts/tvDup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#xrandr --output eDP-1 --primary --mode 1600x900 --pos 0x0 --scale 0.8x0.8 --output DP-1 --off --output HDMI-1 --same-as eDP-1 --mode 1280x720 --scale 1x1
+xrandr --output eDP-1 --primary --mode 1600x900 --pos 0x0 --scale 1x1 --output DP-1 --off --output HDMI-1 --same-as eDP-1 --mode 1280x720 --scale 1.25x1.25
